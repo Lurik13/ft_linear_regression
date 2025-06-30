8740129615835e3b3cd522c0a31091b005faaf77
Lucas Ribette <lucasribette.pro@gmail.com>
2025-06-30 18:33:48 +0200

    Regression lineaire avec une fonction interdite

--- linear_regression.py
+++ linear_regression.py
@@ -1,9 +1,10 @@
-import matplotlib
-matplotlib.use('Agg') # Anti-Grain Geometry
+# import matplotlib
+# matplotlib.use('Agg') # Anti-Grain Geometry
 
 import sys
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy import stats
 
 CHART_IMAGE_NAME = "ft_linear_regression.png"
 
@@ -22,8 +23,8 @@ def parseData(fileName):
                 plt.xlabel(values[0])
                 plt.ylabel(values[1])
             else:
-                xValues.append(values[0])
-                yValues.append(values[1])
+                xValues.append(int(values[0]))
+                yValues.append(int(values[1]))
             linesNumber += 1
         if linesNumber < 2:
             raise Exception("Not enough data.")
@@ -34,10 +35,17 @@ if __name__ == "__main__":
         if len(sys.argv) != 2:
             raise Exception("Please fill in a data file.")
         values = parseData(sys.argv[1])
-        x = np.array(values[0])
-        y = np.array(values[1])
+        x = values[0]
+        y = values[1]
+        print(stats.linregress(x, y))
+        slope, intercept, r, p, std_err = stats.linregress(x, y)
+        def myfunc(x):
+            return slope * x + intercept
+        mymodel = list(map(myfunc, x))
         plt.scatter(x, y)
-        plt.savefig(CHART_IMAGE_NAME)
+        plt.plot(x, mymodel)
+        # plt.savefig(CHART_IMAGE_NAME)
+        plt.show()
         print("You can check \033[4m" + CHART_IMAGE_NAME + "\033[0m.")
     except Exception as e:
         print("\033[38;2;170;0;0;1;4m" + str(e) + "\033[0m")
